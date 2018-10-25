01e70266dde9b077052de58280c7e8650c4c680d
Agnes Brettl <brettl1991@gmail.com>
2018-10-25 16:52:59 +0100

    Cleaning Up our Code

--- shared.js
+++ shared.js
@@ -24,14 +24,19 @@ backdrop.addEventListener("click", function() {
     closeModal();
 });
 
-modalNoButton.addEventListener("click", closeModal);
+if (modalNoButton) {
+    modalNoButton.addEventListener("click", closeModal);
+}
 
 function closeModal() {
     // backdrop.style.display = "none";
     // modal.style.display = "none";
-    modal.classList.remove('open');
+    if (modal) {
+        modal.classList.remove('open');
+    }
     backdrop.classList.remove('open');
 }
+   
 
 toggleButton.addEventListener('click', function() {
     // mobileNav.style.display = 'block';
