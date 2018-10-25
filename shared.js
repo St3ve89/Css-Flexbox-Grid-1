3d36d664eec5f011d2cc7470b864fe7e569bc6cc
Agnes Brettl <brettl1991@gmail.com>
2018-10-25 15:59:05 +0100

    Adding a Side Navigation Bar

--- shared.js
+++ shared.js
@@ -3,9 +3,26 @@ var selectPlanButtons = document.querySelectorAll('.plan button');
 var modal = document.querySelector('.modal');
 
 // console.dir(backdrop);
+// console.log('buttons', selectPlanButtons.length)
 for (var i = 0; i < selectPlanButtons.length; i++) {
-    selectPlanButtons [i].addEventListener('click', function() {
+    selectPlanButtons[i].addEventListener('click', function() {
         modal.style.display = 'block';
         backdrop.style.display = 'block';
     })
 }
+
+
+// let backdrop = document.querySelector('.backdrop');
+// let selectPlanButtons = document.querySelectorAll('.plan button');
+// let modal = document.querySelector('.modal');
+
+// console.dir(backdrop);
+// console.log('buttons', selectPlanButtons.length)
+
+// selectPlanButtons.forEach(element => {
+//     element.addEventListener('click', () => {
+//         console.log('click')
+//         modal.style.display = 'block';
+//         backdrop.style.display = 'block';
+//     })
+// })
