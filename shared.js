229a5b7f96ddda4852eeb7bfb5500aa9225f8663
Agnes Brettl <brettl1991@gmail.com>
2018-10-25 16:21:09 +0100

    Opening and Closing the Hamburger Menu

--- shared.js
+++ shared.js
@@ -1,15 +1,34 @@
-var backdrop = document.querySelector('.backdrop');
-var selectPlanButtons = document.querySelectorAll('.plan button');
-var modal = document.querySelector('.modal');
+var backdrop = document.querySelector(".backdrop");
+var modal = document.querySelector(".modal");
+var modalNoButton = document.querySelector(".modal__action--negative");
+var selectPlanButtons = document.querySelectorAll(".plan button");
+var toggleButton = document.querySelector('.toggle-button');
+var mobileNav = document.querySelector('.mobile-nav');
 
 // console.dir(backdrop);
-// console.log('buttons', selectPlanButtons.length)
 for (var i = 0; i < selectPlanButtons.length; i++) {
-    selectPlanButtons[i].addEventListener('click', function() {
-        modal.style.display = 'block';
-        backdrop.style.display = 'block';
-    })
+    selectPlanButtons[i].addEventListener("click", function() {
+        modal.style.display = "block";
+        backdrop.style.display = "block";
+    });
 }
+
+backdrop.addEventListener("click", function() {
+    mobileNav.style.display = 'none';
+    closeModal();
+});
+
+modalNoButton.addEventListener("click", closeModal);
+
+function closeModal() {
+    backdrop.style.display = "none";
+    modal.style.display = "none";
+}
+
+toggleButton.addEventListener('click', function() {
+    mobileNav.style.display = 'block';
+    backdrop.style.display = 'block';
+});
 
 
 // let backdrop = document.querySelector('.backdrop');
